40d129834281d8745ff8df7060a18f02448f4097
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-25 00:21:55 -0500

    simple Lead schema

---
+++ src/db/schemas.js
@@ -0,0 +1,20 @@
+const { serial } = require('drizzle-orm/mysql-core');
+const { text, pgTable, timestamp, integer } = require('drizzle-orm/pg-core');
+
+const LeadTable = pgTable('leads', {
+  id: serial('id').primaryKey().notNull(),
+  email: text('email'),
+  description: text('description').default('This is my comment'),
+  createdAt: timestamp('created_at').defaultNow(),
+});
+
+module.exports.LeadTable = LeadTable;
+
+// const LeadTable = {
+//   email,
+//   created_at,
+//   description,
+//   source,
+//   first_name,
+//   last_name,
+// };
