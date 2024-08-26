808e2e2936482ebb81fddb7895173f0d89911f0a
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-25 19:09:28 -0500

    app

--- src/index.js
+++ src/index.js
@@ -81,8 +81,8 @@ app.use((req, res, next) => {
   });
 });
 
-module.exports.app = app;
 module.exports.handler = serverless(app);
+module.exports.app = app;
 
 // server-full app
 // app.listen(3400, () => {
